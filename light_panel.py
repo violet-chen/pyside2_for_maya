b410f0a28f2927789535dc6e06b5fa2b5f9eff24
RuiChen <1505291171@qq.com>
2022-12-21 10:10:56 +0800

    灯光面板举例

--- light_panel.py
+++ light_panel.py
@@ -1,4 +1,6 @@
 # coding:utf-8
+# 灯光面板举例
+from functools import partial
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 from PySide2 import QtGui
@@ -11,6 +13,79 @@ def maya_main_window():
     main_window_ptr = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
 
+class CustomColorButton(QtWidgets.QWidget): # 自定义颜色按钮
+
+    color_changed = QtCore.Signal(QtGui.QColor) # 自定义信号
+
+    def __init__(self, color=QtCore.Qt.white, parent=None): # 表示可以类接收一个颜色参数,默认是白色
+        super(CustomColorButton, self).__init__(parent)
+
+        self.setObjectName("CustomColorButton")
+
+        self.create_control()
+
+        self.set_size(50, 14)
+        self.set_color(color) # 设置初始颜色,如果对象有传过来颜色设置则为对象的颜色
+
+    def create_control(self):
+        """ 1. 创建colorSliderGrp """
+        window = cmds.window()
+        self._name = cmds.colorSliderGrp()
+        # print("original name: {0}".format(self._name))
+
+        """ 2. 找到colorSliderGrp控件 """
+        color_slider_obj = omui.MQtUtil.findControl(self._name) # color_slider_obj是一个c++的形式
+        if color_slider_obj:
+            self._color_slider_widget = wrapInstance(long(color_slider_obj), QtWidgets.QWidget)
+        
+            """ 3. 将colorSliderGrp控件的父级设置为这个自定义控件 """
+            main_layout = QtWidgets.QVBoxLayout(self)
+            main_layout.setObjectName("main_layout")
+            main_layout.setContentsMargins(0, 0, 0, 0)
+            main_layout.addWidget(self._color_slider_widget)
+
+            """ 4. 更新colorSliderGrp的control name (之前是属于maya的) """
+            self._name = self._color_slider_widget.objectName()
+            # print("new name: {0}".format(self._name))
+
+            """ 5. 识别或存储 colorSliderGrp的子控件(在必要的时候可以隐藏它) """
+            # children = self._color_slider_widget.children()
+            # for child in children:
+            #     print(child)
+            #     print(child.objectName())
+            # print("---")
+
+            self._slider_widget = self._color_slider_widget.findChild(QtWidgets.QWidget, "slider")
+            if self._slider_widget:
+                self._slider_widget.hide()
+            
+            self._color_widget = self._color_slider_widget.findChild(QtWidgets.QWidget, "port")
+
+            cmds.colorSliderGrp(self._name, e=True, changeCommand=partial(self.on_color_changed))
+
+        cmds.deleteUI(window, window=True)
+          
+    
+    def set_size(self, width, height):
+        self._color_slider_widget.setFixedWidth(width)
+        self._color_widget.setFixedHeight(height)
+    
+    def set_color(self, color):
+        color = QtGui.QColor(color)
+
+        if color != self.get_color():
+            cmds.colorSliderGrp(self._name, e=True, rgbValue=(color.redF(), color.greenF(), color.blueF())) # F代表以浮点数表示
+            self.on_color_changed()
+    
+    def get_color(self):
+        color = cmds.colorSliderGrp(self._name, q=True, rgbValue=True)
+
+        color = QtGui.QColor(color[0] *255, color[1] * 255, color[2] * 255)
+        return color
+      
+    def on_color_changed(self, *args):
+        self.color_changed.emit(self.get_color())
+
 class LightItem(QtWidgets.QWidget):
 
     SUPPORTED_TYPES = ["ambientLight", "directionalLight", "pointLight", "spotLight"]
@@ -21,6 +96,8 @@ class LightItem(QtWidgets.QWidget):
 
         self.setFixedHeight(26)
 
+        self.shape_name = shape_name
+
         self.create_widgets()
         self.create_layout()
         self.create_connections()
@@ -28,12 +105,29 @@ class LightItem(QtWidgets.QWidget):
     def create_widgets(self):
         self.light_type_btn = QtWidgets.QPushButton()
         self.light_type_btn.setFixedSize(20, 20)
+        self.light_type_btn.setFlat(True)
 
         self.visiblity_cb = QtWidgets.QCheckBox()
         
         self.transform_name_label = QtWidgets.QLabel("placeholder")
         self.transform_name_label.setFixedWidth(120)
         self.transform_name_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        light_type = self.get_light_type()
+        if light_type in self.SUPPORTED_TYPES:
+            self.intensity_dsb = QtWidgets.QDoubleSpinBox()
+            self.intensity_dsb.setRange(0.0, 100.0)
+            self.intensity_dsb.setDecimals(3) # 设置数值到小数点后三位
+            self.intensity_dsb.setSingleStep(0.1)
+            self.intensity_dsb.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons) # 设置spinbox没有上下箭头
+
+            self.color_btn = CustomColorButton()
+
+            if light_type in self.EMIT_TYPES:
+                self.emit_diffuse_cb = QtWidgets.QCheckBox()
+                self.emit_specular_cb = QtWidgets.QCheckBox()
+        
+        self.update_values()
     
     def create_layout(self):
         main_layout = QtWidgets.QHBoxLayout(self)
@@ -42,10 +136,79 @@ class LightItem(QtWidgets.QWidget):
         main_layout.addWidget(self.visiblity_cb)
         main_layout.addWidget(self.transform_name_label)
 
+        light_type = self.get_light_type()
+        if light_type in self.SUPPORTED_TYPES:
+            main_layout.addWidget(self.intensity_dsb)
+            main_layout.addSpacing(10)
+            main_layout.addWidget(self.color_btn)
+
+            if light_type in self.EMIT_TYPES:
+                main_layout.addSpacing(34)
+                main_layout.addWidget(self.emit_diffuse_cb)
+                main_layout.addSpacing(50)
+                main_layout.addWidget(self.emit_specular_cb)
+
         main_layout.addStretch()
     
     def create_connections(self):
         pass
+
+    def update_values(self):
+        self.light_type_btn.setIcon(self.get_light_type_icon())
+        self.visiblity_cb.setChecked(self.is_visible())
+        self.transform_name_label.setText(self.get_transform_name())
+
+        light_type = self.get_light_type()
+        if light_type in self.SUPPORTED_TYPES:
+            self.intensity_dsb.setValue(self.get_intensity())
+            self.color_btn.set_color(self.get_color())
+            
+            if light_type in self.EMIT_TYPES:
+                self.emit_diffuse_cb.setChecked(self.emits_diffuse())
+                self.emit_specular_cb.setChecked(self.emits_specular())
+
+    def get_transform_name(self):
+        return cmds.listRelatives(self.shape_name, parent=True)[0]
+
+    def get_attribute_value(self, name, attribute):
+        return cmds.getAttr("{0}.{1}".format(name, attribute))
+
+    def get_light_type(self):
+        return cmds.objectType(self.shape_name)
+
+    def get_light_type_icon(self):
+        light_type = self.get_light_type()
+
+        icon = QtGui.QIcon()
+        if light_type == "ambientLight":
+            icon = QtGui.QIcon(":ambientLight.svg")
+        elif light_type == "directionalLight":
+            icon = QtGui.QIcon(":directionalLight.svg")
+        elif light_type == "pointLight":
+            icon = QtGui.QIcon(":pointLight.svg")
+        elif light_type == "spotLight":
+            icon = QtGui.QIcon(":spotLight.svg")
+        else:
+            icon = QtGui.QIcon(":Light.png")
+
+        return icon
+
+    def is_visible(self):
+        transform_name = self.get_transform_name()
+        return self.get_attribute_value(transform_name, "visibility")
+    
+    def get_intensity(self):
+        return self.get_attribute_value(self.shape_name, "intensity")
+
+    def get_color(self):
+        temp_color = self.get_attribute_value(self.shape_name, "color")[0]
+        return QtGui.QColor(temp_color[0] * 255, temp_color[1] * 255, temp_color[2] * 255)
+
+    def emits_diffuse(self):
+        return self.get_attribute_value(self.shape_name, "emitDiffuse")
+
+    def emits_specular(self):
+        return self.get_attribute_value(self.shape_name, "emitSpecular")
 
 class LightPanel(QtWidgets.QDialog):
 
